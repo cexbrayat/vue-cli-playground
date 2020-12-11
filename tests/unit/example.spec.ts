71766a83e427dbaf16d86555286a2c621b7b44c3
cexbrayat <cedric@ninja-squad.com>
2020-12-11 15:14:04 +0100

    repro: i18n plugin

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,14 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import i18n from '@/i18n'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
+      global: {
+        plugins: [i18n]
+      },
       props: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
